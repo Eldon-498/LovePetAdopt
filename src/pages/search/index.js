e86b9f8a97c3a1a07232d3ea349c24148b025659
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2023-03-13 22:42:13 +0300

    Update index.js

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+//import React, { useState, useEffect, useMemo } from 'react';
 import Hero from '../../components/hero';
 import { getPets } from '../../api/petfinder';
 import Pet from '../../components/pet';

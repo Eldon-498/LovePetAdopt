b8fb3967d69671989bc30286e95ed7236c73d1e8
Daniel Eldon <52330149+Eldon-498@users.noreply.github.com>
2023-03-13 22:40:15 +0300

    Update index.js

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import { getPetTypes } from '../../api/petfinder';
 import Logo from '../../assets/logo.svg';
 import Search from '../search';
